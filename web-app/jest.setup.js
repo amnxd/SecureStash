b0c20f9aa8e032cd99725aa0b83a347ea2ccd6e2
Aman <bhaskarshah4235@gmail.com>
2025-11-12 22:23:59 +0530

    fix(jest): use require in jest.setup.js to avoid ESM import

--- web-app/jest.setup.js
+++ web-app/jest.setup.js
@@ -1,4 +1,3 @@
-// attach jest-dom matchers
-import '@testing-library/jest-dom/extend-expect'
+require('@testing-library/jest-dom/extend-expect')
 // next-router-mock setup; use CommonJS require so Jest can mock
 module.exports = () => {}
